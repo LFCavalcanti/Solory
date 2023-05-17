feb60baa350208e4d2170b0bd0144a4c9e09c55b
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-05-17 18:34:44 -0300

    Enabled absolut paths Gitignore env for dev and prod Small error handling in database connection pool

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,12 +3,19 @@ import { Pool } from 'pg';
 let postgre: Pool;
 let connPort: number = Number(process.env.PGSQL_PORT || '0');
 
-postgre = new Pool({
-  user: process.env.PGSQL_USER,
-  password: process.env.PGSQL_PASSWORD,
-  host: process.env.PGSQL_HOST,
-  port: isNaN(connPort) ? 0 : connPort,
-  database: process.env.PGSQL_DATABASE,
-});
+try {
+  postgre = new Pool({
+    user: process.env.PGSQL_USER,
+    password: process.env.PGSQL_PASSWORD,
+    host: process.env.PGSQL_HOST,
+    port: isNaN(connPort) ? 0 : connPort,
+    database: process.env.PGSQL_DATABASE,
+  });
+} catch (error) {
+  console.error(error);
+  throw new Error(
+    'It was not possible to create a connection pool with the database',
+  );
+}
 
 export default postgre;
