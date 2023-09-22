bf99120f6ad716ea2bf446076e483ba03c71538f
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2023-09-22 17:39:49 -0300

    Implemented Company Address API endpoints

---
+++ src/app/api/internal/companies/[id]/addresses/[addressId]/route.ts
@@ -0,0 +1,145 @@
+import prisma from '@/lib/prisma';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth/next';
+import { companyAddressValidate } from '@/types/Company/tCompanyAddress';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string; addressId: string } },
+) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.id) {
+    return new Response(
+      JSON.stringify({
+        message: 'Invalid Session',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const companyAddress = await prisma.companyAddress.findFirst({
+    where: {
+      id: params.addressId,
+      companyId: params.id,
+      company: {
+        users: {
+          every: {
+            userId: session.user.id,
+          },
+        },
+      },
+    },
+  });
+
+  if (!companyAddress) {
+    return new Response(
+      JSON.stringify({
+        message: `### ID ${params.id} does not exist or insufficient permission`,
+      }),
+      {
+        status: 404,
+      },
+    );
+  }
+  return new Response(JSON.stringify(companyAddress));
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string; addressId: string } },
+) {
+  const session = await getServerSession(authOptions);
+  const body = await request.json();
+  const currentDate = new Date().toISOString();
+
+  if (!session || !session.user.id) {
+    return new Response(
+      JSON.stringify({
+        message: 'Invalid Session',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  if (body.id) {
+    return new Response(
+      JSON.stringify({
+        message: 'Entity Id must not be present in payload body',
+      }),
+      {
+        status: 400,
+      },
+    );
+  }
+
+  //--> CHECAR PAYLOAD SE ESTA COM OS DADOS CORRETOS
+  const validatedSchema = companyAddressValidate.safeParse(body);
+  if (!validatedSchema.success) {
+    console.error(validatedSchema.error);
+    return new Response(
+      JSON.stringify({
+        message: 'Incorrect payload',
+      }),
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const companyAddress = await prisma.companyAddress.findFirst({
+    where: {
+      id: params.addressId,
+      companyId: params.id,
+      company: {
+        users: {
+          every: {
+            userId: session.user.id,
+          },
+        },
+      },
+    },
+  });
+
+  if (!companyAddress) {
+    return new Response(
+      JSON.stringify({
+        message: `### ID ${params.id} does not exist or insufficient permission`,
+      }),
+      {
+        status: 403,
+      },
+    );
+  }
+
+  const updatedCompanyAddress = await prisma.companyAddress.update({
+    where: { id: params.id },
+    data: {
+      ...(validatedSchema.data.isActive == false &&
+        companyAddress.isActive == true && {
+          disabledAt: currentDate,
+        }),
+      ...(validatedSchema.data.isActive == true &&
+        companyAddress.isActive == false && {
+          disabledAt: null,
+        }),
+      ...validatedSchema.data,
+    },
+  });
+
+  if (!updatedCompanyAddress) {
+    return new Response(
+      JSON.stringify({
+        message: 'Error updating company address',
+      }),
+      {
+        status: 409,
+      },
+    );
+  }
+
+  return new Response(JSON.stringify(updatedCompanyAddress));
+}
---
+++ src/app/api/internal/companies/[id]/addresses/route.ts
@@ -0,0 +1,106 @@
+import prisma from '@/lib/prisma';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth/next';
+import { NextRequest, NextResponse } from 'next/server';
+import { newCompanyAddressValidate } from '@/types/Company/tCompanyAddress';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const onlyActive = request.nextUrl.searchParams.get('onlyActive');
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.id) {
+    return new Response(
+      JSON.stringify({
+        message: 'Invalid Session',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const companyAddresses = await prisma.companyAddress.findMany({
+    where: {
+      companyId: params.id,
+      ...(onlyActive === 'true' && { isActive: true }),
+      company: {
+        users: {
+          every: {
+            userId: session.user.id,
+          },
+        },
+      },
+    },
+  });
+  if (!companyAddresses) {
+    return new Response(
+      JSON.stringify({
+        message: `### ID ${params.id} does not exist or insufficient permission`,
+      }),
+      {
+        status: 404,
+      },
+    );
+  }
+
+  return new Response(JSON.stringify(companyAddresses));
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const body = await request.json();
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user.id) {
+    return new Response(
+      JSON.stringify({
+        message: 'Invalid Session',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  //--> CHECAR PAYLOAD SE ESTA COM OS DADOS CORRETOS
+  const validatedSchema = newCompanyAddressValidate.safeParse(body);
+
+  if (!validatedSchema.success) {
+    console.error(validatedSchema.error);
+    return new Response(
+      JSON.stringify({
+        message: 'Incorrect payload',
+      }),
+      {
+        status: 400,
+      },
+    );
+  }
+
+  //--> CRIAR EMPRESA
+  let companyAdress: any = null;
+  try {
+    companyAdress = await prisma.companyAddress.create({
+      data: {
+        companyId: params.id,
+        ...validatedSchema.data,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        message: 'Error creating Company',
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+
+  return NextResponse.json(companyAdress);
+}
