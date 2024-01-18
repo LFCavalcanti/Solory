6316def80b63f146bb942238b0c265a7d750090a
Luiz Fernando Cavalcanti <luizfcavalcanti@gmail.com>
2024-01-18 19:05:41 -0300

    Aditional APIs for blocking and unblocking projects

---
+++ src/app/api/internal/projects/[id]/block/route.ts
@@ -0,0 +1,97 @@
+import prisma from '@/lib/prisma';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth/next';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user.id) {
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Invalid Session',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const userSettings = await prisma.userSettings.findFirst({
+    where: {
+      userId: session.user.id,
+    },
+  });
+
+  const activeCompany = await prisma.company.findFirst({
+    where: {
+      id: userSettings?.activeCompanyId,
+      users: {
+        every: {
+          userId: session.user.id,
+        },
+      },
+    },
+  });
+
+  if (!activeCompany) {
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Error retrieving Company, probably need permissions.',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const [projectData] = await prisma.project.findMany({
+    where: {
+      id: params.id,
+      companyId: activeCompany.id,
+    },
+    orderBy: [
+      {
+        version: 'desc',
+      },
+    ],
+    take: 1,
+  });
+
+  if (!projectData) {
+    return new NextResponse(
+      JSON.stringify({
+        message:
+          'Error retrieving Poject, probably need permissions or invalid ID.',
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const updatedProject = await prisma.project.update({
+    where: {
+      projectId: { id: projectData.id, version: projectData.version },
+    },
+    data: {
+      isActive: false,
+    },
+  });
+
+  if (!updatedProject) {
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Error updating project progress',
+      }),
+      {
+        status: 409,
+      },
+    );
+  }
+
+  return new NextResponse();
+}
